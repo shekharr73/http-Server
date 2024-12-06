8f35f07bf7fed54596fa7f97a2cbc3a007df94a1
shekharr22 <shekharjadhav501@gmail.com>
2024-12-06 22:28:58 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const http = require("http");
 
-const myServer = http.createServer();
+const myServer = http.createServer((req, res) => {
+    console.log("New Req Rec.")
+    res.end("Hello From Server");
+});
+
+myServer.listen(8000, () => console.log("Server Started!"));
