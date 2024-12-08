db1f634a3b1ecde778237e2b9486953d3bedc281
shekharr22 <shekharjadhav501@gmail.com>
2024-12-08 15:38:28 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-    console.log("New Req Rec.");
-    res.end("Hello From Server");
+  const log = `${Date.now()}: ${req.url} : New req Recei\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        res.end("Hey iam shekhar");
+        break;
+      case "/contact":
+        res.end("shekharjadhav501@gmail.com");
+        break;
+    }
+  });
 });
+
+// console.log(req.headers)
+// console.log(req)
 
 myServer.listen(8000, () => console.log("Server Started!"));
